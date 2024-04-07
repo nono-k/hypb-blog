f99f607641b70db554455f949a1fb9e36f04e92d
iwasaki <iwa@cfv.co.jp>
2024-04-08 01:13:26 +0900

    meta情報など追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import { siteMeta } from './src/lib/constants';
+
+const { siteUrl } = siteMeta;
 
 // https://astro.build/config
 export default defineConfig({
+  site: siteUrl,
   prefetch: {
     prefetchAll: true
   },
---
+++ src/lib/constants.js
@@ -0,0 +1,14 @@
+export const siteMeta = {
+  siteTitle: 'нуль',
+  siteDesc: 'Web技術を知る・試す・楽しむためのテックブログ',
+  siteUrl: 'https://hypb.pages.dev',
+  siteType: 'website',
+  siteLocale: 'ja_JP',
+  siteIcon: '/favicon.svg',
+  siteImg: '/ogp.png',
+};
+
+export const categories = [
+  { categoryName: 'Python', categorySlug: 'python' },
+  { categoryName: 'JavaScript', categorySlug: 'javascript' },
+];
