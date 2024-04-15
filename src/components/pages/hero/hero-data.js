7fa7d36043a602bf63d9de0d9f3afe62fd3078a0
iwasaki <iwa@cfv.co.jp>
2024-04-15 16:07:45 +0900

    fix

--- src/components/pages/hero/hero-data.js
+++ src/components/pages/hero/hero-data.js
@@ -2,7 +2,7 @@ export const lists = [
   {
     title: 'テキストが入りますテキストが入ります01',
     slug: 'javascript-programming-basics',
-    img: '/src/assets/images/hero01.jpg',
+    img: '../../assets/images/hero01.jpg',
     color: 'red',
     data: '2024.01.01',
     category: 'Web',
@@ -15,7 +15,7 @@ export const lists = [
   {
     title: 'テキストが入りますテキストが入ります02',
     slug: 'json-python',
-    img: '/src/assets/images/hero02.jpg',
+    img: '../../assets/images/hero02.jpg',
     color: 'blue',
     data: '2024.01.01',
     category: 'Web',
@@ -28,7 +28,7 @@ export const lists = [
   {
     title: 'テキストが入りますテキストが入ります03',
     slug: 'object-sort',
-    img: '/src/assets/images/hero03.jpg',
+    img: '../../assets/images/hero03.jpg',
     color: 'yellow',
     data: '2024.01.01',
     category: 'CSS',
@@ -41,7 +41,7 @@ export const lists = [
   {
     title: 'テキストが入りますテキストが入ります04',
     slug: 'python-loop-processing',
-    img: '/src/assets/images/hero04.jpg',
+    img: '../../assets/images/hero04.jpg',
     color: 'blue',
     data: '2024.01.01',
     category: 'Vue',
